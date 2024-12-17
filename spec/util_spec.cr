b35502fd0f67697985c7823b84cfb59efd86b253
Sandeep Rao <sandeepvasudevarao@gmail.com>
2024-12-17 23:42:46 +0530

    create dedicated test for clubbing keys

--- spec/util_spec.cr
+++ spec/util_spec.cr
@@ -54,8 +54,15 @@ end
 
 describe "chapter_sort" do
   it "sorts correctly" do
-    ary = ["Vol.1 Ch.01", "Vol.1 Ch.02", "Vol.2 Ch. 2.5",
-           "Chapter 3", "Ch. 3", "Ch.04"]
+    ary = ["Vol.1 Ch.01", "Vol.1 Ch.02", "Vol.2 Ch. 2.5", "Ch. 3", "Ch.04"]
+    sorter = ChapterSorter.new ary
+    ary.reverse.sort! do |a, b|
+      sorter.compare a, b
+    end.should eq ary
+  end
+
+  it "clubs Chapter and Ch. together" do
+    ary = ["Vol.1 Chapter 1", "Vol.1 Ch.02", "Vol.2 Ch. 2.5", "Ch. 3", "Ch.04"]
     sorter = ChapterSorter.new ary
     ary.reverse.sort! do |a, b|
       sorter.compare a, b
