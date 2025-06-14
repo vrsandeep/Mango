cdb4a776e5b37cdc9899e77536afbd6960275366
Sandeep Rao <sandeepvasudevarao@gmail.com>
2025-06-14 22:13:40 +0530

    yarn lint fix

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,14 +1,12 @@
 $(() => {
-
   function changePage(page) {
     const url = `${location.protocol}//${location.host}${location.pathname}`;
     if (page < 1) {
       page = 1;
     }
     const newURL = `${url}?${$.param({
-        page: page,
+      page,
     })}`;
     window.location.href = newURL;
   }
-
 });
