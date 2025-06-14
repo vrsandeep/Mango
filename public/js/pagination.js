b3ee928851ab34a566ecaeb088b21ddbd67a0d5d
Sandeep Rao <sandeepvasudevarao@gmail.com>
2025-06-15 03:01:06 +0530

    add pagination to library page (#18)
    targeting issue https://github.com/vrsandeep/Mango/issues/17
    
    Introduces a pagination of 50 mangas per page.

---
+++ public/js/pagination.js
@@ -0,0 +1,10 @@
+const changePage = (page) => {
+  const url = `${location.protocol}//${location.host}${location.pathname}`;
+  if (page < 1) {
+    page = 1;
+  }
+  const newURL = `${url}?${$.param({
+    page,
+  })}`;
+  window.location.href = newURL;
+};
--- src/routes/main.cr
+++ src/routes/main.cr
@@ -39,11 +39,19 @@ struct MainRouter
     get "/library" do |env|
       begin
         username = get_username env
+        page_size = 50
+        current_page = env.params.query["page"]?.try &.to_i || 1
 
         sort_opt = SortOptions.from_info_json Library.default.dir, username
         get_and_save_sort_opt Library.default.dir
 
         titles = Library.default.sorted_titles username, sort_opt
+
+        total_pages = (titles.size / page_size).ceil.to_i
+        if titles.size != 0 && current_page <= total_pages
+          offset = (current_page - 1) * page_size
+          titles = titles[offset, page_size]
+        end
         percentage = titles.map &.load_percentage username
 
         layout "library"
