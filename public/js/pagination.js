a3ec2e71b86d26b443e5e31526bb92e2e7024fc2
Sandeep Rao <sandeepvasudevarao@gmail.com>
2025-06-15 01:20:30 +0530

    use prev and next buttons

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -1,12 +1,10 @@
-$(() => {
-  function changePage(page) {
-    const url = `${location.protocol}//${location.host}${location.pathname}`;
-    if (page < 1) {
-      page = 1;
-    }
-    const newURL = `${url}?${$.param({
-      page,
-    })}`;
-    window.location.href = newURL;
+const changePage = (page) => {
+  const url = `${location.protocol}//${location.host}${location.pathname}`;
+  if (page < 1) {
+    page = 1;
   }
-});
+  const newURL = `${url}?${$.param({
+    page,
+  })}`;
+  window.location.href = newURL;
+};
